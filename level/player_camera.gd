91026bc603a1e74898d9121657b5fe5dad6c1af5
OliverOA <OliverAOakman@gmail.com>
2023-01-08 13:28:53 +1100

    Fix broken camera

--- level/player_camera.gd
+++ level/player_camera.gd
@@ -3,7 +3,7 @@ extends Camera2D
 
 const LERP_SPEED = 0.5
 
-var target_position = 
+# var target_position = 
 
 
 func _ready() -> void:
@@ -16,9 +16,10 @@ func _process(delta: float) -> void:
 
 
 func sharp_goto_position(set_position: Vector2) -> void:
-	target_position = set_position
+	# target_position = set_position
 	position = set_position
 
 
 func smooth_goto_position(set_position: Vector2) -> void:
-	target_position = set_position
+	# target_position = set_position
+	pass
